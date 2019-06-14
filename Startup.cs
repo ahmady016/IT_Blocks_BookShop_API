7bd7bf133ecdd5899233f607a8908165c5350d3c
ahmady016 <ahmady09@gmail.com>
2019-06-14 13:37:44 +0200

    read API Title, Version from appsettings.json

--- Startup.cs
+++ Startup.cs
@@ -5,19 +5,30 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Serialization;
 using Swashbuckle.AspNetCore.Swagger;
 using Newtonsoft.Json;
 using AutoMapper;
 using Web_API.EntityModels;
-using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json.Serialization;
 
 namespace Web_API
 {
   public class Startup
   {
+    private readonly IConfiguration _config;
+    private readonly string _title;
+    private readonly string _version;
+
+    public Startup(IConfiguration config)
+    {
+      _config = config;
+      _title = _config.GetValue<string>("API:Title");
+      _version = _config.GetValue<string>("API:Version");
+    }
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
@@ -31,7 +42,7 @@ namespace Web_API
       // Register a type of DbContext so that it can be used in DI (inside dependent classes' constructors)
       services.AddDbContext<BookStoreContext>();
       // Configure Swagger
-      services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" }));
+      services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = _title, Version = _version }));
       // Allow CORS
       services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
         {
@@ -74,7 +85,7 @@ namespace Web_API
       // Enable middleware to serve generated Swagger as a JSON endpoint.
       app.UseSwagger();
       // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
+      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{_title} {_version}"));
       // Shows UseCors with CorsPolicyBuilder.
       app.UseCors("CorsPolicy");
       // using JWT Authentication
